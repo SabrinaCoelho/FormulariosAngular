cb7ce06c7f84c8ae974c10fad34e582eb3df712f
Sabrina Coelho <sabrinateodoro1019@hotmail.com>
2022-06-29 05:03:32 -0300

    Template form seco

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'template-form',component: TemplateFormComponent },
+  { path: 'data-form',component: DataFormComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'template-form' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
